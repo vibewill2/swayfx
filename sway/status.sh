a499637274f43ca5d0fcb88bb559c39b3a0d5b2f
vibewill2 <vibewill@gmail.com>
2026-03-01 09:40:59 -0300

    Add files via upload

--- sway/status.sh
+++ sway/status.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+
 while true; do
-  # CPU: Pega a carga média de 1 minuto (uptime)
+  # CPU: Pega a carga média de 1 minuto
   CPU=$(uptime | awk -F'load average:' '{print $2}' | cut -d, -f1 | tr -d ' ')
+
+  # MEMÓRIA: Calcula a porcentagem de uso (NOVO)
+  MEM=$(free | grep Mem | awk '{printf "%.0f%%", $3/$2 * 100}')
 
   # Volume: Extrai apenas o primeiro conjunto de números seguido de %
   VOL=$(pactl get-sink-volume @DEFAULT_SINK@ 2>/dev/null | grep -o "[0-9]\+%" | head -1)
@@ -12,8 +16,8 @@ while true; do
   # Data
   DATE=$(date +'%d/%m/%Y %H:%M')
 
-  # O echo agora força a exibição do texto
-  echo " $CPU |  $VOL |  $DATE"
+  # Saída com o novo ícone de Memória 💾
+  echo " $CPU | 💾 $MEM |  $VOL |  $DATE"
   
   sleep 1
 done

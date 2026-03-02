0bb46a04ead547ae9d3a6f780ed40048227412bd
vibewill2 <vibewill@gmail.com>
2026-03-02 11:48:24 -0300

    Add files via upload

--- sway/status.sh
+++ sway/status.sh
@@ -1,23 +1,33 @@
 #!/bin/sh
 
-while true; do
-  # CPU: Pega a carga média de 1 minuto
-  CPU=$(uptime | awk -F'load average:' '{print $2}' | cut -d, -f1 | tr -d ' ')
+# Detecta a interface de rede ativa
+INTERFACE=$(ip route | grep default | awk '{print $5}' | head -n1)
 
-  # MEMÓRIA: Calcula a porcentagem de uso (NOVO)
+while true; do
+  # --- CÁLCULO DE REDE ---
+  R1=$(cat /sys/class/net/$INTERFACE/statistics/rx_bytes)
+  T1=$(cat /sys/class/net/$INTERFACE/statistics/tx_bytes)
+  sleep 1
+  R2=$(cat /sys/class/net/$INTERFACE/statistics/rx_bytes)
+  T2=$(cat /sys/class/net/$INTERFACE/statistics/tx_bytes)
+
+  RX=$(( (R2 - R1) / 1024 ))
+  TX=$(( (T2 - T1) / 1024 ))
+
+  # --- CPU ---
+  CPU=$(cut -d' ' -f1 /proc/loadavg)
+
+  # --- MEMÓRIA ---
   MEM=$(free | grep Mem | awk '{printf "%.0f%%", $3/$2 * 100}')
 
-  # Volume: Extrai apenas o primeiro conjunto de números seguido de %
+  # --- VOLUME ---
   VOL=$(pactl get-sink-volume @DEFAULT_SINK@ 2>/dev/null | grep -o "[0-9]\+%" | head -1)
-  
-  # Se o volume estiver vazio (ex: mudo), coloca 0%
   [ -z "$VOL" ] && VOL="0%"
 
-  # Data
+  # --- DATA ---
   DATE=$(date +'%d/%m/%Y %H:%M')
 
-  # Saída com o novo ícone de Memória 💾
-  echo " $CPU | 💾 $MEM |  $VOL |  $DATE"
-  
-  sleep 1
+  # --- SAÍDA FINAL COM TODAS AS CORES ---
+  # CPU: Ciano | MEM: Verde | REDE: Amarelo | VOL: Magenta | DATA: Laranja/Ouro
+  echo "<span color='#00ffff'></span> $CPU | <span color='#00ff00'>💾</span> $MEM | <span color='#ffff00'>🌐</span> ${RX}K↓ ${TX}K↑ | <span color='#ff00ff'></span> $VOL | <span color='#ffaa00'></span> $DATE"
 done
